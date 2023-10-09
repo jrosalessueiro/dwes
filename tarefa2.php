29a70f9cc078368ec929f196034393f19ede33f0
jrsueiro <jrsueiro@yahoo.es>
2023-10-09 18:39:01 +0200

    Fin tarea 1 UD2 DWES

---
+++ tarefa2.php
@@ -0,0 +1,24 @@
+<?php
+//Tarea 1 de la UD2 DWES empieza el 06/10/2023
+
+
+/*
+Localiza y corrige los errores de este programa PHP:
+```php
+
+<? php
+echo '¿Cómo estás';
+echo 'Estoy bien, gracias.';
+??>
+```
+**/
+
+/*Respuesta: En la línea 9 no se puede dejar el espacio ente <? php sino que debe ser <?php
+            En la línea 12 para cerrar el código debemos poner ?> en lugar de ??>
+
+ El código quedaría: */
+
+echo '¿Cómo estás';
+echo 'Estoy bien, gracias.';
+?>
+ 
