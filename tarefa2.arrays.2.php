0b4afff5d52c73166d64f8d464fd55b4351fb493
joserosalessueiro <jrsueiro@yahoo.es>
2023-10-13 18:36:28 +0200

    tarefa 1.2 da parte dos arrays

---
+++ tarefa2.arrays.2.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 2. Imprime los valores del array asociativo siguiente usando un foreach:*/
+$v[1]=90;
+$v[10] = 200;
+$v['hola']=43;
+$v[9]='e';
+
+foreach ($v as $valor) {
+    echo $valor . "\n";
+}
+?>
