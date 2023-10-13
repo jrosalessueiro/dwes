f57e842e265b369f9f73a1706a1dc0c6b2475bc0
joserosalessueiro <jrsueiro@yahoo.es>
2023-10-13 15:01:01 +0200

    tarefa 2.5.3

---
+++ tarefa2.5.3.php
@@ -0,0 +1,29 @@
+<?php
+
+/*3. (Optativo) Escribe un programa que imprima por pantalla los cuadrados de los 
+30 primeros números naturales.*/ 
+
+$x = 1;
+
+while($x <= 30){
+    $cuadrado = $x * $x;
+    echo "el cuadrado de " . $x . " es: " . $cuadrado ."\n";
+
+    $x++;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*4. Hacer un programa php que calcule el área y el perímetro de un rectángulo
+ (```área=base*altura``` y (```perímetro=2*base+2*altura```). Debéis declarar 
+ las variables base=20 y altura=10. */
+
+?>
